b656f47668b808cb6a77b23b4658be76a2a058d8
Gobinda Das <gobinda993720@gmail.com>
2024-02-10 17:50:11 +0530

    change background_color to backgroundColor

--- js/totty.js
+++ js/totty.js
@@ -9,30 +9,30 @@ function buttonHoverEffect(e, properties) {
 
         properties = properties || {};
         properties.style = properties.style || 1;
-    
+
         if (properties.style === 1) {
             element.classList.add("totty-btn1");
-    
+
             const p1 = createElementWithText("p", element.textContent);
             const p2 = createElementWithText("p", element.textContent);
             const span = document.createElement("span");
 
             for (const prop in properties) {
-                if (prop !== 'style' && prop !== 'colorFrom' && prop !== 'color' && prop !== 'ease' && prop !== 'duration' && prop !== 'backgroundEase' && prop !== 'backgroundDuration' && prop !== 'background_color') {
+                if (prop !== 'style' && prop !== 'colorFrom' && prop !== 'color' && prop !== 'ease' && prop !== 'duration' && prop !== 'backgroundEase' && prop !== 'backgroundDuration' && prop !== 'backgroundColor') {
                     element.style[prop] = properties[prop];
                 }
             }
-    
+
             p1.style.color = `${properties.colorFrom || 'white'}`;
             p2.style.color = `${properties.color || 'gold'}`;
-    
+
             p1.style.transition = `top ${properties.duration || '.5s'} ${properties.ease || 'ease-in-out'}`;
             p2.style.transition = `top ${properties.duration || '.5s'} ${properties.ease || 'ease-in-out'}`;
 
             span.style.transition = `height ${properties.backgroundDuration || '1s'} ${properties.backgroundEase || 'cubic-bezier(0.19, 1, 0.22, 1)'}  ${properties.backgroundDelay || '0.2s'}`;
-            span.style.backgroundColor = `${properties.background_color || 'royalblue'}`;
-             
-    
+            span.style.backgroundColor = `${properties.backgroundColor || 'royalblue'}`;
+
+
             mpbS(p1, p2, span)
             appendElements(element, [p1, p2, span]);
         }
