c9115392abf6f069c11b3a289be5aed3ef873502
Gobinda Das <gobinda993720@gmail.com>
2024-04-05 15:42:14 +0530

    now animateSvg is more powerful

--- js/totty.js
+++ js/totty.js
@@ -2,64 +2,68 @@ const Totty = {
     "animateSvg": animateSvg,
 };
 
-function animateSvg(target, options = {}) {
+function animateSvg(targets, options = {}) {
     if (typeof gsap === 'undefined') {
-        console.warn("GSAP is not defined. Make sure to include GSAP library by either using a CDN or importing it from GSAP.");
-        return;
+      console.error("GSAP is not defined. Make sure to include GSAP library by either using a CDN or importing it from GSAP.");
+      return;
     }
-
+  
     const defaultOptions = {
-        ease: "elastic.out(1,0.3)",
-        duration: 2,
-        offsetLeft: 0,
-        offsetRight: 0,
-        xMultiplier: 1,
-        yMultiplier: 1
+      ease: "elastic.out(1,0.3)",
+      duration: 2,
+      offsetLeft: 0,
+      offsetRight: 0,
+      xMultiplier: 1,
+      yMultiplier: 1
     };
-
+  
     const {
-        ease,
-        duration,
-        offsetLeft,
-        offsetRight,
-        xMultiplier,
-        yMultiplier
-    } = { ...defaultOptions, ...options };
-
-    // Validate target
-    const trg = document.querySelectorAll(target);
-    if (trg.length === 0) {
-        console.error("Target not found.");
+      ease,
+      duration,
+      offsetLeft,
+      offsetRight,
+      xMultiplier,
+      yMultiplier
+    } = {...defaultOptions, ...options};
+  
+    const trg = Array.isArray(targets) ? targets : [targets];
+  
+    trg.forEach(target => {
+      const targetElements = (typeof target === 'string') ? document.querySelectorAll(target) : (target instanceof NodeList || Array.isArray(target)) ? Array.from(target) : [target];
+  
+      if (targetElements.length === 0) {
+        console.error(`Target '${target}' not found.`);
         return;
-    }
-
-    trg.forEach(targetElement => {
-        const { height: sh, width: sw } = targetElement.getBoundingClientRect();
-        gsap.set(targetElement.querySelector("path"), { attr: { d: `M${offsetLeft},${sh / 2} Q${sw / 2},${sh / 2} ${sw - offsetRight},${sh / 2}` } });
-
-        targetElement.onmousemove = e => {
-            const y = mapValue(e.offsetY, [0, sh], [-sh * (yMultiplier - 1), sh * yMultiplier]);
-            const x = mapValue(e.offsetX, [0, sw], [-sw * (xMultiplier - 1), sw * xMultiplier]);
-
-            gsap.to(targetElement.querySelector("path"), {
-                attr: { d: `M${offsetLeft},${sh / 2} Q${x},${y} ${sw - offsetRight},${sh / 2}` },
-                ease: "expo",
-            });
+      }
+  
+      targetElements.forEach(element => {
+        const { height: sh, width: sw } = element.getBoundingClientRect();
+        gsap.set(element.querySelector("path"), { attr: { d: `M${offsetLeft},${sh / 2} Q${sw / 2},${sh / 2} ${sw - offsetRight},${sh / 2}` } });
+  
+        element.onmousemove = e => {
+          const y = mapValue(e.offsetY, [0, sh], [-sh * (yMultiplier - 1), sh * yMultiplier]);
+          const x = mapValue(e.offsetX, [0, sw], [-sw * (xMultiplier - 1), sw * xMultiplier]);
+  
+          gsap.to(element.querySelector("path"), {
+            attr: { d: `M${offsetLeft},${sh / 2} Q${x},${y} ${sw - offsetRight},${sh / 2}` },
+            ease: "expo",
+          });
         };
-
-        targetElement.onmouseleave = () => {
-            gsap.to(targetElement.querySelector("path"), {
-                attr: { d: `M${offsetLeft},${sh / 2} Q${sw / 2},${sh / 2} ${sw - offsetRight},${sh / 2}` },
-                duration: duration,
-                ease: ease,
-            });
+  
+        element.onmouseleave = () => {
+          gsap.to(element.querySelector("path"), {
+            attr: { d: `M${offsetLeft},${sh / 2} Q${sw / 2},${sh / 2} ${sw - offsetRight},${sh / 2}` },
+            duration: duration,
+            ease: ease,
+          });
         };
+      });
     });
-
+  
     function mapValue(inputValue, initialRange, finalRange) {
-        const [initialInputValue, finalInputValue] = initialRange;
-        const [initialOutputValue, finalOutputValue] = finalRange;
-
-        return initialOutputValue + (inputValue - initialInputValue) * (finalOutputValue - initialOutputValue) / (finalInputValue - initialInputValue);
+      const [initialInputValue, finalInputValue] = initialRange;
+      const [initialOutputValue, finalOutputValue] = finalRange;
+  
+      return initialOutputValue + (inputValue - initialInputValue) * (finalOutputValue - initialOutputValue) / (finalInputValue - initialInputValue);
     }
-}
+  }
